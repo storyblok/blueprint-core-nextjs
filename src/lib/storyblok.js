6678bed3b1925cb4ac2425a3c57d31b7a99ed963
Arpit Batra <arpitbatra123@gmail.com>
2025-04-16 14:15:22 +0530

    feat: add environment variables for Storyblok API integration (#3)

--- src/lib/storyblok.js
+++ src/lib/storyblok.js
@@ -5,12 +5,15 @@ import Teaser from "@/components/Teaser";
 import { apiPlugin, storyblokInit } from "@storyblok/react/rsc";
 
 export const getStoryblokApi = storyblokInit({
-  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_TOKEN,
+  accessToken: process.env.STORYBLOK_DELIVERY_API_TOKEN,
   use: [apiPlugin],
   components: {
     page: Page,
     feature: Feature,
     grid: Grid,
     teaser: Teaser,
+  },
+  apiOptions: {
+    endpoint: process.env.STORYBLOK_API_BASE_URL ? `${new URL(process.env.STORYBLOK_API_BASE_URL).origin}/v2` : undefined,
   },
 });
