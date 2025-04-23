d90bf915f429af1c1080f9e44605ed58db064f8c
Manuel Schröder <manuel.schroeder@storyblok.com>
2025-04-23 13:11:57 +0200

    updates

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -3,7 +3,7 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 const Feature = ({ blok }) => {
   return (
     <div className="feature" {...storyblokEditable(blok)}>
-      <strong>{blok.name}</strong>
+      <span>{blok.name}</span>
     </div>
   );
 };
