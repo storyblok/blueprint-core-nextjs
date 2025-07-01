cffd8f83f20e5914ea1e22c47e0b8fa0300e1492
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2025-07-01 17:52:16 +0530

    feat: expose env variable publically to work on client side

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	// Expose the following environment variables to the public bundle.
+	// Only expose variables that are safe to be public. In this case, the Storyblok public access token is not sensitive
+	// and is required for features like live preview to work correctly.
+	env: {
+		STORYBLOK_DELIVERY_API_TOKEN: process.env.STORYBLOK_DELIVERY_API_TOKEN,
+		STORYBLOK_API_BASE_URL: process.env.STORYBLOK_API_BASE_URL,
+	},
+};
 
 export default nextConfig;
