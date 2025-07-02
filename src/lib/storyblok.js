ec93eefa0fb0faf784547e78cf6f5e3b6723dda9
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2025-07-02 14:09:20 +0530

    chore: updated comment text

--- src/lib/storyblok.js
+++ src/lib/storyblok.js
@@ -14,7 +14,7 @@ export const getStoryblokApi = storyblokInit({
 		teaser: Teaser,
 	},
 	apiOptions: {
-		/** Set the correct region for your space. Learn more: https://www.storyblok.com/docs/packages/storyblok-js */
+		/** Set the correct region for your space. Learn more: https://www.storyblok.com/docs/packages/storyblok-js#example-region-parameter */
 		region: 'eu',
 		/** The following code is only required when creating a Storyblok space directly via the Blueprints feature. */
 		endpoint: process.env.STORYBLOK_API_BASE_URL
