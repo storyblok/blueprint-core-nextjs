b13b3f4f826db2fa7e120e07181fec4562d33bf8
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2025-12-17 14:35:51 +0530

    feat: add STORYBLOK_REGION env variable

--- src/lib/storyblok.js
+++ src/lib/storyblok.js
@@ -15,7 +15,7 @@ export const getStoryblokApi = storyblokInit({
 	},
 	apiOptions: {
 		/** Set the correct region for your space. Learn more: https://www.storyblok.com/docs/packages/storyblok-js#example-region-parameter */
-		region: 'eu',
+		region: process.env.STORYBLOK_REGION || 'eu',
 		/** The following code is only required when creating a Storyblok space directly via the Blueprints feature. */
 		endpoint: process.env.STORYBLOK_API_BASE_URL
 			? `${new URL(process.env.STORYBLOK_API_BASE_URL).origin}/v2`
